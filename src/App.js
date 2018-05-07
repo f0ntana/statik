a6bc0375354edd9f732f2e90a9af3a39b3a342ea
Felipe Fontana <felipefontanaf@gmail.com>
2018-05-07 13:40:16 -0400

    change editorconfig, rm linters, add prettier, change class to className

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div class="container">
-                <h1>Statik</h1>
-                <p>A starter kit for static websites.</p>
-                <p>HTML + CSS + JavaScript</p>
+            <div className="container">
+                <h1> Statik </h1> <p> A starter kit for static websites. </p>{' '}
+                <p> HTML + CSS + JavaScript </p>{' '}
             </div>
         );
     }
